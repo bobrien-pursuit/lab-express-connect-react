711a1ade33502a75a2030385b8727da9b4d8bcbd
Brendan O'Brien <bobrien@pursuit.org>
2024-06-27 18:20:06 -0400

    sets up useState

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,3 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
+const Edit = () => {
+    const API = process.env.REACT_APP_URL;
+    const [log, setLogs] = useState({
+        captainName: "",
+        title: "",
+        post: "",
+        mistakesWereMadeToday: false,
+        daysSinceLastCrisis: 0,
+    })
+    
