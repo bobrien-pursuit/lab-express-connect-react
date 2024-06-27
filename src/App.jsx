f3cdacd2a42e4d1836f4150132c9a5889e866673
Brendan O'Brien <bobrien@pursuit.org>
2024-06-27 16:22:20 -0400

    sets up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import Show from './Components/Show';
+import New from './Components/New';
+import Edit from './Components/Edit';
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 function App() {
-  return <div>Hello World</div>;
+const [count, setCount] = useState(0);
+
+
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/logs" replace/>} />
+        <Route path="/logs" element={<Home />} />
+        <Route path="/logs/New" element={<New />} />
+        <Route path="/logs/:index" element={<Show />} />
+        <Route path="/logs/:index/Edit" element={<Edit />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
